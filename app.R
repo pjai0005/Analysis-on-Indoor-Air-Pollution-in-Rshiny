c9a0b2be3427d979d418078dedaad2629dca35d1
Prachi Jaiswal <pjai0005@student.monash.edu>
2021-09-24 02:10:38 +0545

    Basic DT table

--- app.R
+++ app.R
@@ -9,63 +9,95 @@ library(shinythemes)
 tidy_fuels <- read_csv(here("data", "cooking.csv"))
 # you might want to use highlight_key here
 
+
+# UI
+
+
 ui <- fluidPage(
   title = "Indoor Air Pollution",
-  theme = shinytheme("slate"),
+  theme = shinytheme("united"),
   tabsetPanel(
 
-#tab1
+    #tab1
     tabPanel("chart",
-      icon = icon("line-chart"),
-      fluidRow( tags$br(), tags$br()),
-      fluidRow(column(
-          6,
-          offset = 1,
-          # also possible to use plotly here
-          selectizeInput("countries", "Select Countries",
-            choices = NULL,
-            multiple = TRUE
-          )
-        )),
-      fluidRow(column(
-          2,
-          offset = 1,
-          checkboxInput("small_countries",
-            "Hide countries < 1 million",
-            value = FALSE
-          )
-        )
-      ),
-      fluidRow(column(
-        2,
-        offset = 1,
-        checkboxInput("linear_scale",
-                      "Linearize x-axis",
-                      value = FALSE
-        )
-      )
-      ),
-      plotlyOutput("chart", width = "100%"),
-      sliderInput("year",
-        "Year",
-        min = 2000,
-        max = 2016,
-        value = 2016,
-        sep = "",
-        width = "100%"
-      )
-    ),
+             icon = icon("line-chart"),
 
-    #tab1
-    tabPanel("table", dataTableOutput("table"), icon = icon("table")),
 
-    #tab1
-    tabPanel("about", icon = icon("info-circle"))
+             fluidRow( tags$br(), tags$br()),
+             fluidRow(column(
+               6,
+               offset = 1,
+               # also possible to use plotly here
+               selectizeInput("countries", "Select Countries",
+                              choices = NULL,
+                              multiple = TRUE
+               )  )  ),
+
+
+             fluidRow(column(
+               2,
+               offset = 1,
+               checkboxInput("small_countries",
+                             "Hide countries < 1 million",
+                             value = FALSE
+               )  )  ),
+
+
+             fluidRow(column(
+               2,
+               offset = 1,
+               checkboxInput("linear_scale",
+                             "Linearize x-axis",
+                             value = FALSE
+               )  )  ),
+             plotlyOutput("chart", width = "100%"),
+             sliderInput("year",
+                         "Year",
+                         min = 2000,
+                         max = 2016,
+                         value = 2016,
+                         sep = "",
+                         width = "100%"
+             ) ),
+
+    #tab2
+    tabPanel("table", dataTableOutput("table"),
+             icon = icon("table"),
+             fluidRow( offset = 1, tags$br(), tags$br(),
+                       tags$h1("Data Table"),
+                       tags$br(), tags$br()),
+             fluidRow(column(
+               2,
+               offset = 1,
+               radioButtons("tab", "Variables:",
+                                   c("Fuel Comsumption" = "cooking" ,
+                                     "GDP Per Capita" ="gdp_per_capita"),
+                                   selected = "cooking"
+             ) ) ),
+
+             fluidRow( tags$br(), tags$br()),
+             sliderInput("Year",
+                         "Year",
+                         min = 2000,
+                         max = 2016,
+                         value = c(2000, 2016),
+                         width = "90%",
+                         sep = ""
+             )),
+
+    #tab3
+    tabPanel("about", textOutput("about"),
+             icon = icon("info-circle"))
 
 
 
   )
 )
+
+
+
+
+# SERVER
 
 server <- function(input, output, session) {
   # Define reactive expressions here for filtering data
@@ -76,9 +108,55 @@ server <- function(input, output, session) {
                geom_blank())
   })
 
+
   output$table <- renderDataTable({
+
+    if(input$tab == "cooking"){
+
+      tidy_fuels %>%
+        filter(year == input$Year[1] | year == input$Year[2]) %>%
+        select(-c(code, continent, gdp_per_capita, total_population)) %>%
+        pivot_wider(names_from = year ,
+                    values_from = cooking) %>%
+        mutate(Relative = (.[[3]] - .[[2]])/.[[2]]*100 ) %>%
+
+        #select(country, total_population, input$Year[1], input$Year[2], Relative) %>%
+        datatable(escape = FALSE,
+                  caption = htmltools::tags$caption (style = 'caption-side: top; text-align: center;
+                                                              color: black; font-family: Arial;
+                                                              font-size: 150% ;', 'Relative Change in Fule Consumption')) %>%
+        formatRound('Relative', digits = 2) }
+
+
+
+    else{
+      tidy_fuels %>%
+        filter(year == input$Year[1] | year == input$Year[2]) %>%
+        select(-c(code, continent, cooking, total_population)) %>%
+        pivot_wider(names_from = year ,
+                    values_from = gdp_per_capita) %>%
+        mutate(Relative = (.[[3]] - .[[2]])/.[[2]]*100 ) %>%
+
+        #select(country, total_population, input$Year[1], input$Year[2], Relative)%>%
+        datatable(escape = FALSE,
+                  caption = htmltools::tags$caption (style = 'caption-side: top; text-align: center;
+                                                              color: black; font-family: Arial;
+                                                              font-size: 150% ;', 'Realtive Change in GDP per capita')) %>%
+        formatRound('Relative', digits = 2) }
+
+
+  })
+
+
+
+  output$about <- renderText({
     NULL
   })
+
+
+
+
+
 }
 
 
